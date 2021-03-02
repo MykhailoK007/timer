6c3447a9b1cefed8ea701da2814e400ee89a5610
MykhailoK007 <kotmykhailo2000@gmail.com>
2021-03-02 12:11:21 +0200

    created pagination

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import { Login } from '../login/index';
-import './App.scss';
+import { Pagination } from '../pagination/pagination';
+import classes from './app.module.scss';
+
 export const App = () => {
+  const handleChange = (page: number): void => {
+    console.log(`Current page ${page}`);
+  };
   return (
     <>
-      <Login />
+      <Pagination
+        activePage={1}
+        totalItems={20}
+        perPage={5}
+        classes={{ btn: classes.btn, activeBtn: classes.activeBtn }}
+        withActions={true}
+        onChange={handleChange}
+      />
+      <Pagination activePage={1} totalItems={30} perPage={7} withActions={false} onChange={handleChange} />
     </>
   );
 };
--- src/task-2/sideFunctions.ts
+++
@@ -1,3 +0,0 @@
-export const generateId = (): number => {
-  return +(Math.random() * 10000).toString().substring(4, 9);
-};
